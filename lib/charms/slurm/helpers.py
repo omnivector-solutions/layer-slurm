5a4c6f462907218e2496ac7aff458c60bbe7f2d7
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2018-06-08 09:00:10 +0200

    v2 rewrite

--- lib/charms/slurm/helpers.py
+++ lib/charms/slurm/helpers.py
@@ -13,35 +13,35 @@ MUNGE_KEY_TEMPLATE = 'munge.key'
 MUNGE_KEY_PATH = '/etc/munge/munge.key'
 
 
-def render_slurm_config(config):
+def render_slurm_config(context):
     render(source=SLURM_CONFIG_TEMPLATE,
            target=SLURM_CONFIG_PATH,
-           context=config,
-           owner=config.get('slurm_user'),
-           group=config.get('slurm_user'),
+           context=context,
+           owner=context.get('slurm_user'),
+           group=context.get('slurm_user'),
            perms=0o644)
 
 
-def render_munge_key(config):
+def render_munge_key(context):
     render(source=MUNGE_KEY_TEMPLATE,
            target=MUNGE_KEY_PATH,
-           context=config,
+           context=context,
            owner='munge',
            group='munge',
            perms=0o400)
 
 
-def create_spool_dir(config):
-    if not os.path.isdir(config.get('slurmd_spool_dir')):
-        mkdir(path=config.get('slurmd_spool_dir'),
-              owner=config.get('slurm_user'),
-              group=config.get('slurm_user'),
+def create_spool_dir(context):
+    if not os.path.isdir(context.get('slurmd_spool_dir')):
+        mkdir(path=context.get('slurmd_spool_dir'),
+              owner=context.get('slurm_user'),
+              group=context.get('slurm_user'),
               perms=0o750)
 
 
-def create_state_save_location(config):
-    if not os.path.isdir(config.get('state_save_location')):
-        mkdir(path=config.get('state_save_location'),
-              owner=config.get('slurm_user'),
-              group=config.get('slurm_user'),
+def create_state_save_location(context):
+    if not os.path.isdir(context.get('state_save_location')):
+        mkdir(path=context.get('state_save_location'),
+              owner=context.get('slurm_user'),
+              group=context.get('slurm_user'),
               perms=0o750)
