68c4c34f477037992529bbc4cb089f6979718c54
jamesbeedy <jamesbeedy@gmail.com>
2020-04-26 14:18:42 +0000

    test using snap

--- lib/charms/slurm/helpers.py
+++ lib/charms/slurm/helpers.py
@@ -6,16 +6,15 @@ from charmhelpers.core.host import mkdir
 from charmhelpers.core.templating import render
 from charmhelpers.core.hookenv import log
 
-SLURMD_SERVICE = 'slurmd'
-SLURMCTLD_SERVICE = 'slurmctld'
+SLURMD_SERVICE = 'slurm.slurmd'
+SLURMCTLD_SERVICE = 'slurm.slurmctld'
 SLURM_CONFIG_TEMPLATE = 'slurm.conf'
-SLURM_CONFIG_PATH = '/etc/slurm-llnl/slurm.conf'
-SLURM_CONFIG_DIR = '/etc/slurm-llnl'
+SLURM_CONFIG_DIR = f"{os.environ['SNAP_COMMON']}/etc/slurm"
+SLURM_CONFIG_PATH = f'{SLURM_CONFIG_DIR}/slurm.conf'
 
 MUNGE_SERVICE = 'munge'
 MUNGE_KEY_TEMPLATE = 'munge.key'
-MUNGE_KEY_PATH = '/etc/munge/munge.key'
-
+MUNGE_KEY_PATH = f"{os.environ['SNAP_COMMON']}/etc/munge/munge.key"
 
 DEFAULT_INCLUDE_CONFIG = textwrap.dedent("""\
             # This file is automatically distributed to the Slurm cluster by Juju.
@@ -31,15 +30,15 @@ DEFAULT_INCLUDE_CONFIG = textwrap.dedent("""\
         """ % SLURM_CONFIG_PATH)
 
 GRES_CONFIG_TEMPLATE = 'gres.conf'
-GRES_CONFIG_PATH = '/etc/slurm-llnl/gres.conf'
+GRES_CONFIG_PATH = f'{SLURM_CONFIG_DIR}/gres.conf'
 
 
 def render_slurm_config(context, active_controller=False):
     render(source=SLURM_CONFIG_TEMPLATE,
            target=SLURM_CONFIG_PATH,
            context=context,
-           owner=context.get('slurm_user'),
-           group=context.get('slurm_user'),
+           owner="root",
+           group="root",
            perms=0o644)
 
     # Extract clustername from context, create empty config file
@@ -62,37 +61,37 @@ def render_munge_key(context):
     render(source=MUNGE_KEY_TEMPLATE,
            target=MUNGE_KEY_PATH,
            context=context,
-           owner='munge',
-           group='munge',
+           owner="root",
+           group="root",
            perms=0o400)
 
 def render_gres_config(context):
     render(source=GRES_CONFIG_TEMPLATE,
         target=GRES_CONFIG_PATH,
         context=context,
-        owner=context.get('slurm_user'),
-        group=context.get('slurm_user'),
+        owner="root",
+        group="root",
         perms=0o644)
 
 def create_spool_dir(context):
     if not os.path.isdir(context.get('slurmd_spool_dir')):
         mkdir(path=context.get('slurmd_spool_dir'),
-              owner=context.get('slurm_user'),
-              group=context.get('slurm_user'),
+              owner="root",
+              group="root",
               perms=0o750)
 
 
 def create_state_save_location(context):
     if not os.path.isdir(context.get('state_save_location')):
         mkdir(path=context.get('state_save_location'),
-              owner=context.get('slurm_user'),
-              group=context.get('slurm_user'),
+              owner="root",
+              group="root",
               perms=0o750)
 
 
 def get_slurm_version():
     try:
-        sinfo_process = subprocess.Popen(['/usr/bin/sinfo', '-V'], stdout=subprocess.PIPE)
+        sinfo_process = subprocess.Popen(['slurm.sinfo', '-V'], stdout=subprocess.PIPE)
         (sinfo_out, sinfo_err) = sinfo_process.communicate()
         sinfo_process_exit_status = sinfo_process.wait(10)
         if sinfo_process_exit_status != 0:
--- reactive/layer_slurm.py
+++ reactive/layer_slurm.py
@@ -1,42 +1,21 @@
-import importlib
-from charms.slurm.helpers import get_slurm_version
-from charmhelpers.osplatform import get_platform
-
-__platform__ = get_platform()
-module = 'charmhelpers.fetch.%s' % __platform__
-fetch = importlib.import_module(module)
-
-# fetch/centos.py has no equivalent to get_upstream_version
-# query slurm directly instead using sinfo wrapper (both ubuntu/centos)
-from charmhelpers.core.hookenv import status_set
-from charmhelpers.core.hookenv import application_version_set
-
 from charms.reactive import when_not
 from charms.reactive import set_state
 
-# Packages
-if __platform__ == 'ubuntu':
-    pkg_install = fetch.apt_install
-    packages = ['slurm-wlm']
-elif __platform__ == "centos":
-    pkg_install = fetch.install
-    packages = ['slurm-slurmd']
+from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import application_version_set
+
+from charms.slurm.helpers import get_slurm_version
 
 
-@when_not('slurm.installed')
-def install_slurm():
-    status_set('maintenance', 'installing slurm packages')
-
-    # Install packages
-    pkg_install(packages)
-
-    # TODO: Query if installation was successful, then set flag,
-    # status and version accordingly.
-
+@when('snap.slurm.installed')
+@when_not('slurm.base.available')
+def set_slurm_version():
     # Set Slurm version
     slurm_version = get_slurm_version()
     if slurm_version is not None:
         application_version_set(slurm_version)
-        set_state('slurm.installed')
     else:
-        status_set('maintenance', 'failed to install slurm packages, will retry')
+        status_set('blocked', 'cannot get slurm version')
+        return
+    status_set('active', '')
+    set_state('slurm.base.available')
